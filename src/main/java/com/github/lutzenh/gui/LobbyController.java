78ab65e13cb52ec7324b86981b8250c68d7f62d2
Coen VIser <c.r.visser@st.hanze.nl>
2020-04-03 14:19:39 +0200

    Controller voor screens toegevoegd

---
+++ src/main/java/com/github/lutzenh/gui/LobbyController.java
@@ -0,0 +1,62 @@
+package com.github.lutzenh.gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+public class LobbyController extends AnchorPane {
+    @FXML private ListView currentPlayers;
+
+    public void initialize() {
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+        currentPlayers.setItems(names);
+    }
+
+    @FXML
+    public void OrthelloVsAI(ActionEvent actionEvent) throws MalformedURLException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(new File("src/main/resources/lobby.fxml").toURI().toURL());
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    public void OrthelloVsPlayer(ActionEvent actionEvent) throws MalformedURLException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(new File("src/main/resources/lobby.fxml").toURI().toURL());
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    public void TickVsPlayer(ActionEvent actionEvent) throws MalformedURLException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(new File("src/main/resources/lobby.fxml").toURI().toURL());
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    public void TickVsAI(ActionEvent actionEvent) throws MalformedURLException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(new File("src/main/resources/lobby.fxml").toURI().toURL());
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+}
