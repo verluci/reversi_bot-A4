df51d78b38b09d055758f3dc5fdd2760f991b97d
LutzenH <46445220+LutzenH@users.noreply.github.com>
2020-04-06 10:29:31 +0200

    Add RandomMoveAIAgent

--- src/main/java/com/github/verluci/reversi/game/agents/FirstMoveAIAgent.java
+++ src/main/java/com/github/verluci/reversi/game/agents/FirstMoveAIAgent.java
@@ -1,6 +1,5 @@
 package com.github.verluci.reversi.game.agents;
 
-import com.github.verluci.reversi.game.Game;
 import com.github.verluci.reversi.game.GameBoard;
 import com.github.verluci.reversi.game.Tile;
 import com.github.verluci.reversi.game.TileState;
@@ -10,13 +9,6 @@ import com.github.verluci.reversi.game.TileState;
  */
 public class FirstMoveAIAgent extends AIAgent {
     /**
-     * The constructor for TicTacToeAIEntity
-     */
-    public FirstMoveAIAgent() {
-        super();
-    }
-
-    /**
      * @param board The board on which the optimal tile should be found on.
      * @return The first possible move it can find.
      */
@@ -24,14 +16,5 @@ public class FirstMoveAIAgent extends AIAgent {
     protected Tile findOptimalMove(GameBoard board) {
         var possibleMoves = board.getTilesWithState(TileState.POSSIBLE_MOVE);
         return possibleMoves.get(0);
-    }
-
-    /**
-     * Sets the game this entity should play in.
-     * @param game The game this entity should play in.
-     */
-    @Override
-    public void setGame(Game game) {
-        super.setGame(game);
     }
 }
